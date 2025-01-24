9677653760e313124457e5ba9e4e759190d54372
khkim09 <09ricky@naver.com>
2025-01-24 16:37:05 +0900

    add cinemachine

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,8 @@ using UnityEngine.Rendering;
 public class Player : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] private GameObject mainCamera;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float moveAmount;
-
-    void Awake()
-    {
-        if (mainCamera == null)
-        {
-            mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<GameObject>();
-        }
-    }
-
-    void LateUpdate()
-    {
-        mainCamera.transform.position = this.transform.position + new Vector3(0, 0, -10f);
-    }
 
     void Update()
     {
